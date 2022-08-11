a08ee5b1b894bf6de5951b5832a3951de7877601
prosperchihimba <prosperchihimba@gmail.com>
2022-08-11 11:42:44 +0300

    [Update]: Re-design mui table

--- src/componets/MainDash/Table/Table.jsx
+++ src/componets/MainDash/Table/Table.jsx
@@ -6,33 +6,54 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
+import './Table.css'
 
-function createData(name, calories, fat, carbs, protein) {
-    return { name, calories, fat, carbs, protein };
+function createData(name, trackingId, date, status) {
+    return { name, trackingId, date, status };
 }
 
 const rows = [
-    createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-    createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-    createData('Eclair', 262, 16.0, 24, 6.0),
-    createData('Cupcake', 305, 3.7, 67, 4.3),
-    createData('Gingerbread', 356, 16.0, 49, 3.9),
+    createData('Frozen yoghurt', 15933444, "2 March 22", "Approved"),
+    createData('Ice cream sandwich', 15933444, "2 March 22", "Pending"),
+    createData('Eclair', 15933444, "2 March 22", "Approved"),
+    createData('Cupcake', 15933444, "2 March 22", "Delivered"),
 ];
+
+const makeStyles = (status) => {
+    if (status === "Approved") {
+        return {
+            background: 'rgb(145 254 159 / 47%)',
+            color: 'green'
+        }
+    } else if (status === "Pending") {
+        return {
+            background: '#ffadad8f',
+            color: 'red'
+        }
+    } else {
+        return {
+            background: '#59bfff',
+            color: 'white'
+        }
+    }
+} 
 
 export default function DashboardTable() {
     return (
         <div className="Table">
             <h3>Recent Orders</h3>
 
-            <TableContainer component={Paper}>
+            <TableContainer
+                component={Paper}
+                style={{boxShadow: '0px 13px 20px 0px #80808029'}}
+            >
                 <Table sx={{ minWidth: 650 }} aria-label="simple table">
                     <TableHead>
                     <TableRow>
-                        <TableCell>Dessert (100g serving)</TableCell>
-                        <TableCell align="left">Calories</TableCell>
-                        <TableCell align="left">Fat&nbsp;(g)</TableCell>
-                        <TableCell align="left">Carbs&nbsp;(g)</TableCell>
-                        <TableCell align="left">Protein&nbsp;(g)</TableCell>
+                        <TableCell>Product</TableCell>
+                        <TableCell align="left">Tracking ID</TableCell>
+                        <TableCell align="left">Date</TableCell>
+                        <TableCell align="left">Status</TableCell>
                     </TableRow>
                     </TableHead>
                     <TableBody>
@@ -44,10 +65,13 @@ export default function DashboardTable() {
                         <TableCell component="th" scope="row">
                             {row.name}
                         </TableCell>
-                        <TableCell align="left">{row.calories}</TableCell>
-                        <TableCell align="left">{row.fat}</TableCell>
-                        <TableCell align="left">{row.carbs}</TableCell>
-                        <TableCell align="left">{row.protein}</TableCell>
+                        <TableCell align="left">{row.trackingId}</TableCell>
+                        <TableCell align="left">{row.date}</TableCell>
+                        <TableCell align="left">
+                            <span className="status" style={makeStyles(row.status)}>
+                                {row.status}
+                            </span>
+                        </TableCell>
                         </TableRow>
                     ))}
                     </TableBody>
