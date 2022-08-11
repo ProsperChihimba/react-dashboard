d0824fbfa09e5318b026dbe1f8668fa588489af6
prosperchihimba <prosperchihimba@gmail.com>
2022-08-11 04:25:00 +0300

    [Add]: Side bar initialize

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import './App.css'
+import './App.css';
+import Sidebar from './componets/Sidebar/Sidebar';
 
 function App() {
-return (
-<div className="App">
-  <div class="AppGlass">
-
-  </div>
-</div>
-);
+  return (
+    <div className="App">
+        <div class="AppGlass">
+          <Sidebar />
+        </div>
+    </div>
+  );
 }
 
 export default App;
